b33c4830f4c9f052e889912cf920f4b893824a43
Wyatt-James <bomberbeast44@gmail.com>
2024-05-25 11:49:28 -0500

    Improve Documentation of Shader Binaries

--- src/pc/gfx/gfx_3ds_shader_binaries.h
+++ src/pc/gfx/gfx_3ds_shader_binaries.h
@@ -2,8 +2,13 @@
 #define GFX_3DS_SHADER_BINARIES_H
 
 /*
- *  This file should ONLY contain SHBIN references, as provided by bin2s.
- *  You should not use this file directly under most circumstances.
+ * This file should ONLY contain SHBIN references, as provided by bin2s.
+ * You should not use this file directly under most circumstances.
+ * 
+ * In case anybody gets confused, bin2s outputs .o files directly into the
+ * build/src directory, and these .o files simply contain binary representations
+ * of their files, with variable names adapted from the input file's name as
+ * seen below.
  */
 
 #include <stdint.h>
