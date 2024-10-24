00cf4a498ce3325a2cf0dc266edade743f53ff97
Wyatt-James <bomberbeast44@gmail.com>
2024-10-23 23:34:13 -0500

    Fix DSP Buf Address Cast Caused by VSCode weirdness. It is worth noting that osConvertVirtToPhys is actually incorrect here; the simple cast is correct.

--- src/pc/audio/audio_3ds.c
+++ src/pc/audio/audio_3ds.c
@@ -125,7 +125,7 @@ static void audio_3ds_play_internal(const uint8_t *src, size_t len_total, size_t
     // DSP_FlushDataCache is slow if AppCpuLimit is set high for some reason.
     // svcFlushProcessDataCache is much faster and still works perfectly.
     // DSP_FlushDataCache(dst, len_total);
-    svcFlushProcessDataCache(CUR_PROCESS_HANDLE, (Handle) dst, len_total);
+    svcFlushProcessDataCache(CUR_PROCESS_HANDLE, (__3ds_u32) dst, len_total);
     
     // Actually play the data
     sDspBuffers[sNextBuffer].nsamples = len_total / 4;
