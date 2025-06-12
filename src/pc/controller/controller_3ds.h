5e0d93eba0ebee7954fb90a1f223f588254bbba3
Wyatt-James <bomberbeast44@gmail.com>
2025-06-11 22:47:54 -0500

    Add TODO

--- src/pc/controller/controller_3ds.h
+++ src/pc/controller/controller_3ds.h
@@ -5,7 +5,7 @@
 
 #include "controller_api.h"
 
-extern __3ds_u32 controller_3ds_force_hold; // Forces N64 buttons to be held. See hid.h for definitions.
+extern __3ds_u32 controller_3ds_force_hold; // Forces N64 buttons to be held. See hid.h for definitions. WYATT_TODO this should be a u16.
 extern struct ControllerAPI controller_3ds;
 
 #endif
